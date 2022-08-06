e09b39b02e30c67666caa9de11d10a4802af05b7
David Caldwell <david@porkrind.org>
2022-08-05 20:15:13 -0700

    syncron.rs: Remove `syncron db sync` as the only old db in existence has now been updated to sqlite

--- syncron.rs
+++ syncron.rs
@@ -20,7 +20,6 @@ Usage:
   syncron -c <job-cmd>
   syncron [-h] [-v...] exec -n <name> [-i <id>] [--timeout=<timespec>] [--server=<server-url>] <job-cmd>
   syncron [-h] [-v...] serve [--db=<path>] [--port=<port>]
-  syncron [-h] [-v...] db sync [--db=<path>]
 
 Options:
   -h --help              Show this message.
@@ -47,8 +46,6 @@ struct Args {
     flag_server:  Option<String>,
     cmd_exec:     bool,
     cmd_serve:    bool,
-    cmd_db:       bool,
-    cmd_sync:     bool,
     arg_job_cmd:  String,
 }
 
@@ -96,112 +93,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
         crate::db::MIGRATOR.run(&sql).await.expect("migrate");
         let serve = async { serve::serve(args.flag_port, db_path.clone().into(), false).await.map_err(|e| format!("serve failed: {}", e)) };
         tokio::join!(serve).0?;
-    } else
-
-    if args.cmd_db && args.cmd_sync {
-        let db_path: PathBuf = args.flag_db.ok_or("missing --db or SYNCRON_DB environment variable")?.into();
-        let sql = sqlx::SqlitePool::connect(&format!("{}/{}", &db_path.to_string_lossy(), "syncron.sqlite3")).await?;
-
-        let jobs_path = db_path.join("jobs");
-
-        use std::path::{Path,PathBuf};
-        pub fn dirs(dir: &Path) -> Result<Vec<String>, Box<dyn Error>> {
-            if !dir.exists() { return Ok(vec![]); }
-            Ok(std::fs::read_dir(dir)?
-               .filter_map(|entry| entry.ok())
-               .filter(|entry| match entry.metadata() { Ok(m) => m.file_type().is_dir(), _ => false })
-               .map(|entry| entry.file_name().to_string_lossy().into())
-               .collect())
-        }
-
-        pub struct OldServerJob {
-            pub user: String,
-            pub id: String,
-        }
-
-        let mut jobs: Vec<OldServerJob> = vec![];
-        for user in dirs(&jobs_path)? {
-            jobs.append(&mut dirs(&jobs_path.join(&user))?.iter()
-                        .map(|id| OldServerJob{user: user.clone(), id: id.to_string() })
-                        .collect())
-        }
-
-        for job in jobs.iter() {
-            #[derive(Debug, serde::Serialize, serde::Deserialize)]
-            pub struct JobInfo {
-                pub name: String,
-            }
-            let info_path = jobs_path.join(&job.user).join(&job.id).join("info");
-            if !info_path.is_file() { warn!("No info file!"); continue; }
-            let info: JobInfo = serde_json::from_slice(&std::fs::read(info_path)?)?;
-
-            sqlx::query!("INSERT INTO user (name) VALUES (?) ON CONFLICT DO NOTHING", job.user)
-                .execute(&sql).await?;
-            let user_id = sqlx::query!("SELECT user_id FROM user WHERE name = ?", job.user)
-                .fetch_one(&sql).await?
-                .user_id;
-            let job_name = info.name;
-            sqlx::query!("INSERT INTO job (user_id, id, name) values (?, ?, ?) ON CONFLICT DO NOTHING", user_id, job.id, job_name)
-                .execute(&sql).await?;
-
-            let job_id = sqlx::query!("SELECT job_id FROM job WHERE user_id = ? and id = ?", user_id, job.id)
-                    .fetch_one(&sql).await?
-                    .job_id;
-
-            let job_path = jobs_path.join(&job.user).join(&job.id);
-            let paths = dirs(&job_path)?;
-            let mut runs = paths.iter()
-               .filter_map(|name| chrono::DateTime::parse_from_rfc3339(name).map(|d| (d,name)).ok())
-               .collect::<Vec<(chrono::DateTime<chrono::FixedOffset>, &String)>>();
-            runs.sort_by(|a,b| a.0.cmp(&b.0));
-            #[derive(Debug, PartialEq)]
-            pub struct OldServerRun {
-                pub date: chrono::DateTime<chrono::FixedOffset>,
-                pub run_id: String,
-                pub client_id: Option<u128>,
-            }
-            let runs: Vec<OldServerRun> = runs.into_iter().map(|run_date| OldServerRun{ date: run_date.0, run_id: run_date.1.clone(), client_id: None }).collect();
-
-            for run in runs.into_iter() {
-                let run_path = job_path.join(&run.run_id);
-                let info_path = run_path.join("info");
-                if !info_path.is_file() { warn!("No info file: {}", info_path.to_string_lossy()); continue }
-                use crate::maybe_utf8::MaybeUTF8;
-                #[derive(Debug, serde::Serialize, serde::Deserialize, Clone)]
-                pub struct OldServerRunInfo {
-                    pub cmd:    String,
-                    pub env:    Vec<(MaybeUTF8,MaybeUTF8)>,
-                    pub end:    Option<chrono::DateTime<chrono::Utc>>,
-                    pub status: Option<serve::ExitStatus>,
-                }
-                let info = {
-                    let info: Result<OldServerRunInfo,_> = serde_json::from_slice(&std::fs::read(&info_path).map_err(|e| wrap(&e, &format!("read {}", info_path.to_string_lossy())))?)
-                        .map_err(|e| wrap(&e, &format!("parse {}", info_path.to_string_lossy())));
-                    if let Err(e) = info { warn!("{}", e); continue; }
-                    info.unwrap()
-                };
-
-                print!("\r\x1b[2KImporting run for {} [{}]...", job.id, run.run_id);
-                use std::io::Write;
-                let _ = std::io::stdout().flush();
-
-                let env = serde_json::to_string(&info.env)?;
-                let log_path = run_path.join("log");
-                let log_path = log_path.as_os_str().to_str().ok_or(format!("bad path: {:?}", log_path))?.to_owned();
-                let start = run.date.timestamp_millis();
-                let end = info.end.map(|t| t.timestamp_millis());
-                let status = serde_json::to_string(&info.status)?;
-                sqlx::query!(r"INSERT INTO run (job_id, cmd, env, log, start, end, status)
-                                      VALUES (?, ?, ?, ?, ?, ?, ?)
-                                      ON CONFLICT DO UPDATE SET cmd    = excluded.cmd,
-                                                                env    = excluded.env,
-                                                                log    = excluded.log,
-                                                                end    = excluded.end,
-                                                                status = excluded.status",
-                             job_id, info.cmd, env, log_path, start, end, status)
-                    .execute(&sql).await?;
-            }
-        }
     }
 
     Ok(())
